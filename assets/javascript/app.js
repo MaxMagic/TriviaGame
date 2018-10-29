3d358d6c916ee80fd2cbf45ec5649a01164e52b1
MaxMagic <Lennox.E.David@gmail.com>
2018-10-29 16:25:12 -0400

    v.2

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+/* Trivia Game
+5 total questions
+NBA themed
+Questions will live in object literals with the correct answer outsiude of the question */
+const myQuestions = [
+  {
+    question: "How long is a regulation NBA game?",
+    answers: {
+      a: "40 minutes",
+      b: "48 minutes",
+      c: "60 minutes"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "How many seconds does an NBA team have to shoot the ball?",
+    answers: {
+      a: "30 seconds",
+      b: "10 seconds",
+      c: "24 seconds"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Wilt Chamberlain once scored 100 points in a game. Who has the second-highest point total in a game?",
+    answers: {
+      a: "Kobe Bryant",
+      b: "LeBron James",
+      c: "Devin Booker",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Which NBA team has the most titles?",
+    answers: {
+      a: "The Chicago Bulls",
+      b: "The Los Angeles Lakers",
+      c: "The Boston Celtics",
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What former player was the inspiration for the NBA logo?",
+    answers: {
+      a: "Elgin Baylor",
+      b: "Jerry West",
+      c: "Wilt Chamberlain",
+    },
+    correctAnswer: "b"
+  }
+];
